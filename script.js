6889fdca98607bd97f70bc8b7e7e6dda87598fc8
Chih-Hsuan Huang <chihhsuan_huang@tk3c.com>
2020-06-15 18:10:18 +0800

    修改顯示清單; some refactor

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
 
 
-var locStorage = window.localStorage;
-
-if (locStorage.length <= 0){
-    for (var i = 0 ; i < 10; i ++){
-        var aData = {}
-        locStorage.setItem("BoxNo"+i,"N" );
-    }
-}
 
 document.addEventListener("DOMContentLoaded", function(){
-    var t = document.getElementById("tableData").getElementsByTagName("tbody")[0];
+    //var _lsTotal=0,_xLen,_x;for(_x in localStorage){ if(!localStorage.hasOwnProperty(_x)){continue;} _xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen; console.log(_x.substr(0,50)+" = "+ (_xLen/1024).toFixed(2)+" KB")};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
+    
+    fetchData();
 
-    for (var  i = 0 ; i < locStorage.length ; i ++){
-        var key = locStorage.key(i);
-        var tr = t.insertRow();
-        tr.id=key;
-        var td1  = tr.insertCell(0);
-        var td2  = tr.insertCell(1);
-
-        var chk = locStorage.getItem(key);
-
-        switch (chk){
-            case "Y":
-                tr.classList.add("chk_Y");
-                break;
-            case "N":
-                tr.classList.add("chk_N");
-                break;
-            case "E":
-                tr.classList.add("chk_E");
-                break;
+    function fetchData(){
+        var locStorage = window.localStorage;
+        var t = document.getElementById("tableData").getElementsByTagName("tbody")[0];
+        if (locStorage.length <= 0){
+            for (var i = 0 ; i < 100; i ++){
+                locStorage.setItem("BoxNo"+i,"N" );
+            }
         }
 
-        td1.appendChild(document.createTextNode(chk));
-        td2.appendChild(document.createTextNode(key));
+        for (var  i = 0 ; i < locStorage.length ; i ++){
+            var key = locStorage.key(i);
+            var tr = t.insertRow();
+            tr.id=key;
+            var td1  = tr.insertCell(0);
+            var td2  = tr.insertCell(1);
+    
+            var chk = locStorage.getItem(key);
+    
+            switch (chk){
+                case "Y":
+                    tr.className = "chk chk_Y"
+                    break;
+                case "N":
+                    tr.className = "chk chk_N";
+                    break;
+                case "E":
+                    tr.className = "chk chk_E";
+                    break;
+            }
+    
+            //td1.appendChild(document.createTextNode(chk));
+            td1.appendChild(document.createTextNode(key));
+        }
+        filterTableByChk('N');
     }
 
 
-    function filterTableByChk(eventBtn, chk){
-        var allY = document.getElementsByClassName('chk_Y');
-        var allN = document.getElementsByClassName('chk_N');
-        var allE = document.getElementsByClassName('chk_E');
+    // function refreshCount(){
+    //     var locStorage = window.localStorage;
 
-        if(eventBtn.classList.contains("active")){
-            for (var i = 0; i < allY.length; i ++) {
-                allY[i].style.display =  "table-row";
-            }
-            for (var i = 0; i < allN.length; i ++) {
-                allN[i].style.display = "table-row";
-            }
-            for (var i = 0; i < allE.length; i ++) {
-                allE[i].style.display = "table-row";
-            }
-            eventBtn.classList.remove("active");
-            return;
-        }
-        var allActiveBtns = document.getElementsByClassName('active');
-        for (var i = 0; i < allActiveBtns.length; i ++) {
-            allActiveBtns[i].classList.remove("active");
-        }
+    //     document.getElementById("countN");
+    //     document.getElementById("countY");
+    //     document.getElementById("countE");
+    // }
 
-        eventBtn.classList.add("active")
+    function clearTable(){
+        var table = document.getElementById("tableData").getElementsByTagName("tbody")[0];
+        table.innerHTML = "";
+    }
 
-        for (var i = 0; i < allY.length; i ++) {
-            allY[i].style.display = (chk=="N" || chk=="E") ? 'none' : "table-row";
+    function filterTableByChk(chk){
+        var allChk = document.getElementsByClassName('chk');
+        for (var c of allChk){
+            c.style.display =  (c.classList.contains("chk_" + chk) ) ? null : 'none';
         }
-
-        for (var i = 0; i < allN.length; i ++) {
-            allN[i].style.display = (chk == "Y" || chk == "E") ? 'none' : "table-row";
-        }
-
-        for (var i = 0; i < allE.length; i ++) {
-            allE[i].style.display = (chk == "N" || chk == "Y") ? 'none' : "table-row";
-        }
-        
     }
 
     // --------------btn input event------------------
     var btnFilterN = document.getElementById("btnFilterN");
     var btnFilterY =  document.getElementById("btnFilterY");
     var btnFilterE =  document.getElementById("btnFilterE");
-    btnFilterN.onclick = function(e){filterTableByChk(e.target, "N")};
-    btnFilterY.onclick = function(e){filterTableByChk(e.target, "Y")};
-    btnFilterE.onclick = function(e){filterTableByChk(e.target, "E")};
+    btnFilterN.onclick = function(e){filterTableByChk("N")};
+    btnFilterY.onclick = function(e){filterTableByChk("Y")};
+    btnFilterE.onclick = function(e){filterTableByChk("E")};
+
+    var btnExit = document.getElementById("btnExit");
+    btnExit.onclick = function(e){
+        clearTable();
+        window.localStorage.clear();    
+    };
+
+    var btnFetch = document.getElementById("btnFetch");
+    btnFetch.onclick = function(e){
+        window.localStorage.clear();
+        clearTable();
+        fetchData();
+    };
+
     // ----------------Txt input event----------------
     var txtInput = document.getElementById("txtBoxno");
     txtInput.addEventListener("keyup", function(e) {
@@ -91,8 +91,13 @@ document.addEventListener("DOMContentLoaded", function(){
         if (e.keyCode === 13) {
             // Cancel the default action
             e.preventDefault();
-            //document.getElementById("myBtn").click();
-            var boxno = e.target.value;
+            
+            var locStorage = window.localStorage;
+            var boxno = e.target.value.trim();
+
+            if (!boxno.trim()){
+                return;
+            }
             var chk = locStorage.getItem(boxno);
             var chk_status = "E";
             if (chk == "N"){
@@ -110,14 +115,14 @@ document.addEventListener("DOMContentLoaded", function(){
                 td2.appendChild(document.createTextNode(boxno));
             }
             var tr =  document.getElementById(boxno);
-            if (tr){
-                tr.setAttribute("class","")
-            }
-            tr.classList.add("chk_" + chk_status);
-            var td = tr.cells[0];
-            td.innerHTML = chk_status;
+            tr.style.display = tr.style.display != 'none' ?  'none' : null;
+            tr.removeAttribute("class");
+            tr.className = "chk chk_" + chk_status;
+            // var td = tr.cells[0];
+            // td.innerHTML = chk_status;
             locStorage.setItem(boxno, chk_status);
+            e.target.value = "";
+            e.target.focus();
         }
     });
-
 });
