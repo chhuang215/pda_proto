319ff9fb29b2e4735ac97e5adc8639b44370f73d
Chih-Hsuan Huang <chihhsuan_huang@tk3c.com>
2020-06-16 15:57:01 +0800

    test: migrate to vue

--- script.js
+++ script.js
@@ -1,124 +1,79 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     //let _lsTotal=0,_xLen,_x;for(_x in localStorage){ if(!localStorage.hasOwnProperty(_x)){continue;} _xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen; console.log(_x.substr(0,50)+" = "+ (_xLen/1024).toFixed(2)+" KB")};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
-    
-    //Get API from here
-    function fetchData(){
-        let locStorage = window.localStorage;
-        
-        if (locStorage.length <= 0){
-            for (let i = 0 ; i < 150; i ++){
-                locStorage.setItem("boxno"+i,"N" );
+    var vueApp = new Vue({
+        el: '#train3',
+        data: {
+            trainBoxList: [],
+            chk_active: "N",
+        },
+        computed: {
+            countN: function () {
+                return this.trainBoxList.filter(function (tb) { return tb.CHK == "N" }).length
+            },
+            countY: function () {
+                return this.trainBoxList.filter(function (tb) { return tb.CHK == "Y" }).length
+            },
+            countE: function () {
+                return this.trainBoxList.filter(function (tb) { return tb.CHK == "E" }).length
             }
-        }
-        displayData();
-    }
+        },
+        methods: {
+            generateData: function () {
+                //Get API from here
+                let locStorage = window.localStorage;
+                if (locStorage.length <= 0) {
+                    for (let i = 0; i < 150; i++) {
+                        let aData = {};
+                        aData["BOX_NO"] = "boxno" + i;
+                        aData["CHK"] = "N";
+                        aData["BRANCH_ID"] = "";
+                        this.trainBoxList.push(aData);
+                    }
+                    locStorage.setItem("trainBoxList", JSON.stringify(this.trainBoxList));
+                }
+                else {
+                    this.trainBoxList = JSON.parse(locStorage.getItem("trainBoxList"));
+                }
+            },
+            updateChk: function (e) {
+                let txtBox = e.target;
+                let boxno = txtBox.value.trim();
+                if (!boxno) {
+                    return;
+                }
+                let tbIndex = this.trainBoxList.findIndex(function (tb) {
+                    return tb.BOX_NO == boxno
+                });
 
-    //Iterate localStorage data onto table element
-    function displayData(){
-        let t = document.getElementById("tableData").getElementsByTagName("tbody")[0];
-        let locStorage = window.localStorage;
-        for (let  i = 0 ; i < locStorage.length ; i ++){
-            let key = locStorage.key(i);
-            let tr = t.insertRow();
-            tr.id=key;
-            
-            let td  = tr.insertCell(0);
-            tr.insertCell();
-            let chk = locStorage.getItem(key);
-            tr.className = "chk chk_"+chk;
-            td.innerHTML = key;
-        }
-    }
-
-    function refreshCount(){
-        let t = document.getElementById("tableData");
-        document.getElementById("countN").innerHTML = t.querySelectorAll(".chk_N").length ;
-        document.getElementById("countY").innerHTML = t.querySelectorAll(".chk_Y").length;
-        document.getElementById("countE").innerHTML = t.querySelectorAll(".chk_E").length;
-    }
-
-    function clearTable(){
-        document.getElementById("tbodyData").innerHTML = "";
-    }
-
-    function displayTableByChk(btnClicked, chk){
-
-        let activeBtns = document.getElementsByClassName("active");
-        for (let ab of activeBtns) {ab.classList.remove("active")};
-
-        btnClicked.classList.add("active");
-
-        let allChk = document.getElementsByClassName('chk');
-        for (let c of allChk){
-            let clist = c.classList;
-            if (clist.contains("chk_" + chk) ){
-                clist.remove("hidden");
+                if (tbIndex >= 0) {
+                    this.trainBoxList[tbIndex].CHK = this.trainBoxList[tbIndex].CHK == "Y" ? "N" : "Y";
+                }
+                else {
+                    this.trainBoxList.push({
+                        BOX_NO: boxno,
+                        CHK: "E",
+                        BRANCH_ID: ""
+                    });
+                }
+                window.localStorage.setItem("trainBoxList", JSON.stringify(this.trainBoxList));
+                txtBox.focus();
+                txtBox.value = "";
             }
-            else clist.add("hidden");
-        }
-    }
-
-    // --------------btn input event------------------
-    let btnFilterN = document.getElementById("btnFilterN");
-    let btnFilterY =  document.getElementById("btnFilterY");
-    let btnFilterE =  document.getElementById("btnFilterE");
-    btnFilterN.onclick = function(e){
-        displayTableByChk(e.target, "N")
-    };
-    btnFilterY.onclick = function(e){
-        displayTableByChk(e.target, "Y")
-    };
-    btnFilterE.onclick = function(e){
-        displayTableByChk(e.target, "E")
-    };
-
-    let btnExit = document.getElementById("btnExit");
-    btnExit.onclick = function(e){
-        clearTable();
-        window.localStorage.clear();    
-        refreshCount();
-    };
-
-    let btnFetch = document.getElementById("btnFetch");
-    btnFetch.onclick = function(e){
-        window.localStorage.clear();
-        clearTable();
-        fetchData();
-        refreshCount();
-        document.getElementById("btnFilterN").click();
-    };
-
-    // ----------------Txt input event----------------
-    let txtInput = document.getElementById("txtBoxno");
-    txtInput.addEventListener("keyup", function(e) {
-     // Number 13 is the "Enter" key on the keyboard
-        if (e.keyCode === 13) {
-            // Cancel the default action
-            e.preventDefault();
-            let boxno = e.target.value.trim();
-            if (!boxno){
-                return;
+            ,
+            clearTable: function () {
+                this.trainBoxList = [];
+            },
+            clearData: function () {
+                this.clearTable();
+                window.localStorage.clear();
+            },
+            refetch: function () {
+                this.clearData();
+                this.generateData();
             }
-            let locStorage = window.localStorage;
-            let tr = document.getElementById(boxno);
-            if (!tr){
-                tr = document.getElementById("tbodyData").insertRow();
-                tr.id=boxno;
-                let td = tr.insertCell();
-                tr.insertCell();
-                td.innerHTML = boxno;
-            }
-            let chk = locStorage.getItem(boxno);
-            let chk_status = chk == "N" ? "Y" : chk == "Y" ? "N" : "E";
-            tr.className = document.getElementById("btnFilter" + chk_status).classList.contains("active") ? "chk chk_" + chk_status : "hidden chk chk_" + chk_status;
-            locStorage.setItem(boxno, chk_status);
-            e.target.value = "";
-            e.target.focus();
-            refreshCount();
-        }
+        },
+        mounted: function () {
+            this.generateData();
+        },
     });
-    // INIT operation
-    fetchData();
-    refreshCount();
-    document.getElementById("btnFilterN").click();
 });
