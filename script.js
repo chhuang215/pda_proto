a24f7e4d7fa47adf4f144468dc6fc33ae46567d8
Chih-Hsuan Huang <chihhsuan_huang@tk3c.com>
2020-06-24 17:52:26 +0800

    rename variable; added processing indicator instead of changing background color

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //var mockBoxNo = [ 'A911018010371', 'A911018010521', 'A911018010522', 'A911018010821', 'A911018010853', 'A911018010914', 'A911018010915', 'A911018010916', 'A911018010917', 'A911018010918', 'A911018010919', 'A911018010920', 'A911018010921', 'A911018010922', 'A911018010923', 'A911018010940', 'A911018011027', 'A911018011141', 'A911018011145', 'A911018011170', 'A911018011171', 'A911018011176', 'A911018011271', 'A911018011358', 'A911018011366', 'A911018011374', 'A911018011375', 'A911018011381', 'A911018011382', 'A911018011383', 'A911018011384', 'A911018011385', 'A911018011386', 'A911018011387', 'A911018011388', 'A911018011389', 'A911018011390', 'A911018011391', 'A911018011392', 'A911018011393', 'A911018011394', 'A911018011501', 'A911018011502', 'A911018011503', 'A911018011504', 'A911018011505', 'A911018011506', 'A911018011507', 'A911018011508', 'A911018011509', 'A911018011510', 'A911018011511', 'A911018011512', 'A911018011513', 'A911018011514', 'A911018011515', 'A911018011516', 'A911018011517', 'A911018011518', 'A911018011519', 'A911018011520', 'A911018011521', 'A911018011522', 'A911018011523', 'A911018011524', 'A911018011525', 'A911018011526', 'A911018011527', 'A911018011528', 'A911018011529', 'A911018011530', 'A911018011531', 'A911018011532', 'A911018011533', 'A911018011534', 'A911018011535', 'A911018011536', 'A911018011537', 'A911018011538', 'A911018011539', 'A911018011540', 'A911018011541', 'A911018011542', 'A911018011543', 'A911018011544', 'A911018011545', 'A911018011546', 'A911018011547', 'A911018011548', 'A911018011549', 'A911018011550', 'A911018011551', 'A911018011552', 'A911018011553', 'A911018011554', 'A911018011555', 'A911018011556', 'A911018011557', 'A911018011558' ];
-
 axios.defaults.baseURL = "http://10.254.247.103:18718/"
 axios.defaults.headers.common['X-Powered-TK'] = "5kvS2m5rNtltyOoqkMlNpUzWRmrtpemh7f8jDvHdsiA=";
-//let _lsTotal=0,_xLen,_x;for(_x in localStorage){ if(!localStorage.hasOwnProperty(_x)){continue;} _xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen; console.log(_x.substr(0,50)+" = "+ (_xLen/1024).toFixed(2)+" KB")};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
+let _lsTotal=0,_xLen,_x;for(_x in localStorage){ if(!localStorage.hasOwnProperty(_x)){continue;} _xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen;};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
 var vueApp = new Vue({
     el: '#train3',
     data: {
@@ -10,8 +9,9 @@ var vueApp = new Vue({
         workNo:"",
         macNo:"",
         carNo:"",
-        shpStore:"",     
-        lstBoxData: [],
+        shpStore:"",
+        boxData: [],
+        boxData: [],
         chk_active: "N",
         processingBoxes: [],
         fetchingData: false,
@@ -19,7 +19,6 @@ var vueApp = new Vue({
     },
     watch: {
         storeNo: function(val){
-            console.log("watched")
             localStorage.setItem('StoreNo', val);
         },
         carNo: function(val){
@@ -37,18 +36,19 @@ var vueApp = new Vue({
     },
     computed: {
         countN: function () {
-            return this.lstBoxData.filter(function (tb) { return tb.CHK == "N" }).length
+            return this.boxData.filter(function (tb) { return tb.CHK == "N" }).length
         },
         countY: function () {
-            return this.lstBoxData.filter(function (tb) { return tb.CHK == "Y" }).length
+            return this.boxData.filter(function (tb) { return tb.CHK == "Y" }).length
         },
         countE: function () {
-            return this.lstBoxData.filter(function (tb) { return tb.CHK == "E" }).length
+            return this.boxData.filter(function (tb) { return tb.CHK == "E" }).length
         }
     },
     methods: {
         fetchData: function () {
             var vueThis = this;
+            vueThis.debugMessage = "";
             if (localStorage.length < 1) 
             {
                 if (vueThis.fetchingData) return;
@@ -65,7 +65,9 @@ var vueApp = new Vue({
                         vueThis.macNo = trainBoxResult.MacNo;
                         vueThis.carNo = trainBoxResult.CarNo;
                         vueThis.shpStore = trainBoxResult.ShpStore;
-                        vueThis.lstBoxData = trainBoxResult.BoxData;
+                        vueThis.boxData = trainBoxResult.BoxData;
+                        vueThis.boxData = trainBoxResult.BoxData;
+                        localStorage.setItem('boxData', JSON.stringify(vueThis.boxData));
                         for (let box of trainBoxResult.BoxData){
                             boxNoIndex.push(box.BoxNo);
                             localStorage.setItem(box.BoxNo, JSON.stringify(box))
@@ -80,7 +82,6 @@ var vueApp = new Vue({
                     .then(function () {
                         console.log("done");
                         vueThis.fetchingData = false;
-                        vueThis.debugMessage += " finish fetching";
                     });  
             }
             else {
@@ -90,8 +91,8 @@ var vueApp = new Vue({
                 this.shpStore = localStorage.getItem("ShpStore");
                 let boxNoIndex = JSON.parse(localStorage.getItem("boxNoIndex"));
                 for (let boxno of boxNoIndex) {
-                    let boxData = localStorage.getItem(boxno)
-                    this.lstBoxData.push(JSON.parse(boxData));
+                   // let b = localStorage.getItem(boxno)
+                    this.boxData.push(JSON.parse(localStorage.getItem(boxno)));
                 }
             }
         },
@@ -102,10 +103,10 @@ var vueApp = new Vue({
                 return;
             }
             this.processingBoxes.push(boxno);
-            let tbIndex = this.lstBoxData.findIndex(function (tb) {return tb.BoxNo == boxno});
+            let tbIndex = this.boxData.findIndex(function (tb) {return tb.BoxNo == boxno});
 
             if (tbIndex >= 0) {
-                let boxListResult = this.lstBoxData[tbIndex];
+                let boxListResult = this.boxData[tbIndex];
                 if (boxListResult.CHK != "E") {
                     let flag = boxListResult.CHK == "Y" ? "N" : "Y"
 
@@ -146,7 +147,7 @@ var vueApp = new Vue({
                     CHK: "E",
                     StoreNo: ""
                 }
-                this.lstBoxData.push(newData);
+                this.boxData.push(newData);
                 let lst = JSON.parse(localStorage.getItem("boxNoIndex"));
                 lst.push(boxno);
                 localStorage.setItem("boxNoIndex" , JSON.stringify(lst));
@@ -179,7 +180,7 @@ var vueApp = new Vue({
         }
         ,
         clearTable: function () {
-            this.lstBoxData = [];
+            this.boxData = [];
         },
         clearData: function () {
             this.clearTable();
